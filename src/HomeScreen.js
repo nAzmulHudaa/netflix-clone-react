9de0691aedc91a49443cf848982a9e140144e074
nftproswap <nftproswap@gmail.com>
2021-06-16 03:57:43 +0600

    Home page done

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,12 +2,45 @@ import React from 'react';
 import './HomeScreen.css';
 import Nav from './Nav';
 import Banner from './Banner';
+import requests from './Request';
+import Row from './Row';
 
 const HomeScreen = () => {
     return (
         <div className='homeScreen'>
             <Nav></Nav>
             <Banner></Banner>
+            <Row
+            title = "NETFLIX ORIGINALS"
+            fetchUrl = {requests.fetchNetflixOriginals}
+            isLargeRow
+            />
+            <Row
+            title = "Trending Now"
+            fetchUrl = {requests.fetchTranding}
+             />
+            <Row
+            title = "Action Movies"
+            fetchUrl = {requests.fetchActionMovies} 
+            />
+            <Row 
+            title = "Comedy Movies"
+            fetchUrl = {requests.fetchComedyMovies}
+            />
+            <Row
+            title = "Horror Movies"
+            fetchUrl ={requests.fetchHorrorMovies} 
+            />
+            <Row
+            title = "Romance Movies"
+            fetchUrl = {requests.fetchRomanceMovies} 
+            />
+            <Row
+            title = "Documentaries"
+            fetchUrl = {requests.fetchDocumentaries} 
+            />
+
+
          
         </div>
     );
